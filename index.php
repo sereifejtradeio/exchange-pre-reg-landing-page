48359b8b4f68cb7317b338c1f76290adec93898f
Stavros Ereifej <stavros.ereifej@trade.io>
2018-05-11 10:07:45 +0300

    Added Google Tag Manager

--- index.php
+++ index.php
@@ -16,47 +16,59 @@
    ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-   <head>
-      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-      <title>trade.io - Beta Exchange Platform - &amp; Pre-Registration</title>
-      <meta name="robot" content="index,follow">
-      <meta name="copyright" content="Copyright © 2016 trade.io. All Rights Reserved.">
-      <meta name="author" content="">
-      <meta name="revisit-after" content="30">
-      <link rel="shortcut icon" type="image/x-icon" href="">
-      <link rel="stylesheet" type="text/css" href="css/style.css" media="screen">
-      <link rel="stylesheet" type="text/css" href="css/forms.css" media="screen">
-      <!--Font Awesome -->
-      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
-      <!--AOS library-->
-      <link href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css" rel="stylesheet">
-      <!-- Jquery Framework -->
-      <script type="text/javascript" src="//code.jquery.com/jquery.min.js"></script>
-      <!--Particles Entrty-->
-      <script type="text/javascript" src="https://rawgit.com/JulianLaval/canvas-particle-network/master/particle-network.min.js"></script>
-      <!--Google reCaptcha-->
-      <script src="https://www.google.com/recaptcha/api.js?onload=myCallBack&render=explicit" async defer></script>
-      <!-- charts -->
-      <script type="text/javascript" src="js/Chart.js"></script>
-      <!-- transform2d -->
-      <script type="text/javascript" src="js/plugins/jquery.transform2d.js"></script>
-      <!-- transform3d -->
-      <script type="text/javascript" src="js/plugins/jquery.transform3d.js"></script>
-      <!-- Jquery Easing -->
-      <script type="text/javascript" src="js/easing.js"></script>
-      <!-- trade.io Checkout-->
-      <script type="text/javascript" src="dashboard/js/functions.js"></script>
-      <!-- Custom -->
-      <script type="text/javascript" src="js/custom.js"></script>
-      <!-- Waypoints -->
-      <script type="text/javascript" src="js/waypoints.min.js"></script>
-      <!-- trade.io API-->
-      <script type="text/javascript" src="api2/api.js"></script>
-      <!--AOS library-->
-      <script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
-   </head>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <title>trade.io - Beta Exchange Platform - &amp; Pre-Registration</title>
+        <meta name="robot" content="index,follow">
+        <meta name="copyright" content="Copyright © 2016 trade.io. All Rights Reserved.">
+        <meta name="author" content="">
+        <meta name="revisit-after" content="30">
+        <link rel="shortcut icon" type="image/x-icon" href="">
+        <link rel="stylesheet" type="text/css" href="css/style.css" media="screen">
+        <link rel="stylesheet" type="text/css" href="css/forms.css" media="screen">
+        <!--Font Awesome -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+        <!--AOS library-->
+        <link href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css" rel="stylesheet">
+        <!-- Jquery Framework -->
+        <script type="text/javascript" src="//code.jquery.com/jquery.min.js"></script>
+        <!--Particles Entrty-->
+        <script type="text/javascript" src="https://rawgit.com/JulianLaval/canvas-particle-network/master/particle-network.min.js"></script>
+        <!--Google reCaptcha-->
+        <script src="https://www.google.com/recaptcha/api.js?onload=myCallBack&render=explicit" async defer></script>
+        <!-- charts -->
+        <script type="text/javascript" src="js/Chart.js"></script>
+        <!-- transform2d -->
+        <script type="text/javascript" src="js/plugins/jquery.transform2d.js"></script>
+        <!-- transform3d -->
+        <script type="text/javascript" src="js/plugins/jquery.transform3d.js"></script>
+        <!-- Jquery Easing -->
+        <script type="text/javascript" src="js/easing.js"></script>
+        <!-- trade.io Checkout-->
+        <script type="text/javascript" src="dashboard/js/functions.js"></script>
+        <!-- Custom -->
+        <script type="text/javascript" src="js/custom.js"></script>
+        <!-- Waypoints -->
+        <script type="text/javascript" src="js/waypoints.min.js"></script>
+        <!-- trade.io API-->
+        <script type="text/javascript" src="api2/api.js"></script>
+        <!--AOS library-->
+        <script src="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.js"></script>
+
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-WLHR9B4');</script>
+        <!-- End Google Tag Manager -->
+    </head>
    <body cz-shortcut-listen="true">
+       <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WLHR9B4"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
       <div id="mobile-detection"></div>
       <!-- notification -->
       <!--<div class="popup-notification error font-semibold"></div>-->

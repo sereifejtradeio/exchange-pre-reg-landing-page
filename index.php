4be1fb33f21716f1a53aeaa45237e7990f5884ef
Stavros Ereifej <stavros.ereifej@trade.io>
2018-05-16 12:09:50 +0300

    reverted

--- index.php
+++ index.php
@@ -1,18 +1,18 @@
 <?php 
 
-    require 'vendor/autoload.php';
-    use GeoIp2\Database\Reader;
+    //require 'vendor/autoload.php';
+    //use GeoIp2\Database\Reader;
 
     // This creates the Reader object, which should be reused across
     // lookups.
-    $reader = new Reader('GeoIP/GeoLite2-Country.mmdb');
+    //$reader = new Reader('GeoIP/GeoLite2-Country.mmdb');
 
-    $ip = $_SERVER['REMOTE_ADDR'];
+    $ip = getUserIP();
 
-    $record = $reader->country($ip);
+    //$record = $reader->country($ip);
 
-    $country_isoCode = $record->country->isoCode; // US
-    $country_name = $record->country->name; // United States
+    //$country_isoCode = $record->country->isoCode; // US
+    //$country_name = $record->country->name; // United States
 
     $autoLoadLanguage = array(
         'China',
@@ -61,7 +61,7 @@
     $i18n->init();
 
     if( in_array($country_name, $autoLoadLanguage) ) {
-        $lang = strtolower($country_isoCode);
+        //$lang = strtolower($country_isoCode);
     } else {
         $lang = isset($_GET['lang']) ? $_GET['lang'] : 'en';
     }
@@ -93,9 +93,9 @@
             $captcha_language = $lang;
     }
 
-    if( !isset($_GET['lang']) ) {
-        header("Location: ?lang={$lang}");
-    }
+//    if( !isset($_GET['lang']) ) {
+//        header("Location: ?lang={$lang}");
+//    }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" dir="<?php echo $lang == 'ar' ? 'rtl' : 'ltr' ?>">
